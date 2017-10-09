a19b4ecadc637025ce2c1bee4752b8085772db4c
flacode <flavianshemerirwe@gmail.com>
2017-10-09 10:31:43 +0300

    FIXTURE Add initial version of database

--- run.py
+++ run.py
@@ -13,11 +13,15 @@ POSTGRES = {
     'host': 'localhost',
     'port': '5432',
 }
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/\
+    %(db)s' % POSTGRES
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 # set debug to true for development purposes
 app.config['DEBUG'] = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-# %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SECRET_KEY'] = 'this-really-needs-to-be-changed'
 db.init_app(app)  # connect sqlalchmey to app
 
